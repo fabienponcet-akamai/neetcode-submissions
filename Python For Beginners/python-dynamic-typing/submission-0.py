81123e46e8895ae470d7850de0bd108890faa472
fabienponcet-akamai <fponcet@akamai.com>
2026-06-15 15:43:29 +0200

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 5
+print(type(variable))
+variable = 5.1
+print(type(variable))
+variable = True
+print(type(variable))
+variable = "True"
+print(type(variable))
+variable = [1,2]
+print(type(variable))
